59da4a58265c756e1596832f51fdb1dedead936a
Petros Koutsolampros <commits@pklampros.io>
2024-06-12 22:22:36 +0300

    Update licences and headers to the SPDX spec and enable the reuse tool for the pre-commit hook

--- pafmath.h
+++ pafmath.h
@@ -1,26 +1,8 @@
-
+// SPDX-FileCopyrightText: 1996-2011 Alasdair Turner (a.turner@ucl.ac.uk)
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 // Paf Template Library --- a set of useful C++ templates
-//
-// Copyright (c) 1996-2011, Alasdair Turner (a.turner@ucl.ac.uk)
-//
-//-----------------------------------------------------------------------------
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
-//  See the lgpl.txt file for details
-//-----------------------------------------------------------------------------
 
 // a collection of math functions
 

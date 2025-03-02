81c379a409d67f2ab139601b766fe5b75b3b109b
Petros Koutsolampros <commits@orange-vertex.net>
2025-03-02 09:30:42 +0200

    Make sure -Wpadding is always enabled

--- bspnodetree.hpp
+++ bspnodetree.hpp
@@ -14,11 +14,11 @@ class BSPNodeTree {
     BSPNode *m_root = nullptr;
     bool m_built = false;
 
-  public:
-    BSPNodeTree()
-        : m_root(nullptr), m_built(false){
+    [[maybe_unused]] unsigned _padding0 : 3 * 8;
+    [[maybe_unused]] unsigned _padding1 : 4 * 8;
 
-                           };
+  public:
+    BSPNodeTree() : m_root(nullptr), m_built(false), _padding0(0), _padding1(0) {}
     void resetBSPtree() { m_built = false; }
     void destroy() {
         if (m_root) {

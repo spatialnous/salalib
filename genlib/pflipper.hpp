42065d745d5173e37d5f68acaca89f82afaee0f4
Petros Koutsolampros <commits@orange-vertex.net>
2025-03-03 19:52:22 +0200

    Initialise pflipper padding in the ctor

--- genlib/pflipper.hpp
+++ genlib/pflipper.hpp
@@ -17,12 +17,12 @@ template <class T> class pflipper {
 
   public:
     pflipper() : m_parity(0) {}
-    pflipper(const T &a, const T &b) : m_parity(0) {
+    pflipper(const T &a, const T &b) : m_parity(0), _padding0() {
         m_contents[0] = a;
         m_contents[1] = b;
         std::memset(_padding0, 0, sizeof(_padding0)); // Zero out the padding
     }
-    pflipper(const pflipper &f) : m_parity(f.m_parity) {
+    pflipper(const pflipper &f) : m_parity(f.m_parity), _padding0() {
         m_contents[0] = f.m_contents[0];
         m_contents[1] = f.m_contents[1];
         std::memset(_padding0, 0, sizeof(_padding0)); // Zero out the padding

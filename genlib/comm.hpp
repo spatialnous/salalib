fbc4262d888a20a00b0c08a1b206df164f864a35
Petros Koutsolampros <commits@orange-vertex.net>
2025-03-28 05:12:00 +0200

    Remove the icommunicator
    It should be implemented by interested interfaces instead

--- genlib/comm.hpp
+++ genlib/comm.hpp
@@ -138,51 +138,6 @@ class Communicator {
     virtual void logInfo(const std::string &message) const = 0;
 };
 
-// this is a simple version of the Communicator which can be used for
-// an interface
-
-class ICommunicator : public Communicator {
-    friend class IComm; // IComm is found in idepthmap.h
-                        //
-  protected:
-    mutable size_t m_numSteps;
-    mutable size_t m_numRecords;
-    mutable size_t m_step;
-    mutable size_t m_record;
-    //
-  public:
-    ICommunicator() : m_numSteps(0), m_numRecords(0), m_step(0), m_record(0) {
-        m_deleteFlag = true;
-    } // note: an ICommunicator lets IComm know that it should delete it
-    ~ICommunicator() override {}
-    void CommPostMessage(size_t m, size_t x) const override;
-
-    void logError(const std::string &message) const override { std::cerr << message << std::endl; }
-    void logWarning(const std::string &message) const override {
-        std::cerr << message << std::endl;
-    }
-    void logInfo(const std::string &message) const override { std::cout << message << std::endl; }
-};
-
-inline void ICommunicator::CommPostMessage(size_t m, size_t x) const {
-    switch (m) {
-    case Communicator::NUM_STEPS:
-        m_numSteps = x;
-        break;
-    case Communicator::CURRENT_STEP:
-        m_step = x;
-        break;
-    case Communicator::NUM_RECORDS:
-        m_numRecords = x;
-        break;
-    case Communicator::CURRENT_RECORD:
-        m_record = x;
-        break;
-    default:
-        break;
-    }
-}
-
 // a helpful little function...
 // This function is used exclusively to update the communicators at specific intervals (set in
 // milliseconds by the timeout argument). Typical usage: Create a time_t t1 and pass to this
